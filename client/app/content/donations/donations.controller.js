b51adf7deaa522b7ae236f5c6b6dff94b8226111
Annie Luc <Annie Luc>
2016-02-14 18:18:30 -0500

    Add controller to donations page

--- client/app/content/donations/donations.controller.js
+++ client/app/content/donations/donations.controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
 angular.module('womenWorkingWithWomenApp')
-  .controller('DonationsCtrl', function($scope){
-    $scope.title = "Donate"
-    $scope.subtitle = "Be the change you want to see"
-  })
+  .controller('DonationsCtrl', function(){
+  });
