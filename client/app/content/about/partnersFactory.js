8ff5c6e5c7c44bfba2d479ea36219bac67108d44
bmtreuherz <bmtreuherz@ufl.edu>
2016-02-15 20:57:25 -0500

    got some of partners done

---
+++ client/app/content/about/partnersFactory.js
@@ -0,0 +1,46 @@
+
+angular.module('womenWorkingWithWomenApp')
+  .factory('Partners', function(){
+    return  [
+      {
+        name: "Adventist Community Service",
+        imgUrl: "http://i.imgur.com/jTymVLE.gif"
+      },
+      {
+        name: "Bread of the Mighty Foodbank",
+        imgUrl: "http://i.imgur.com/lzBPA2S.jpg"
+      },
+      {
+        name: "Domino's Pizza",
+        imgUrl: "http://i.imgur.com/6jiC5Gf.png"
+      },
+      {
+        name: "Enactus",
+        imgUrl: "http://i.imgur.com/BQrkFcD.png"
+      },
+      {
+        name: "Florida Dept of Health",
+        imgUrl: "http://i.imgur.com/SoIPd4d.gif"
+      },
+      {
+        name: "Florida Health",
+        imgUrl: "http://i.imgur.com/uXlFOwZ.png"
+      },
+      {
+        name: "Adventist Community Service",
+        imgUrl: "http://i.imgur.com/jTymVLE.gif"
+      },
+      {
+        name: "Adventist Community Service",
+        imgUrl: "http://i.imgur.com/jTymVLE.gif"
+      },
+      {
+        name: "Adventist Community Service",
+        imgUrl: "http://i.imgur.com/jTymVLE.gif"
+      },
+      {
+        name: "Adventist Community Service",
+        imgUrl: "http://i.imgur.com/jTymVLE.gif"
+      },
+    ]
+  });
