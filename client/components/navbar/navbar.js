1bead4372c4120d4a03a3c425d1a01920ac6a15f
Bradley Treuherz <bradleymtreuherz@gmail.com>
2016-02-16 00:40:31 -0500

    Added highlighted link for active route

--- client/components/navbar/navbar.js
+++ client/components/navbar/navbar.js
@@ -1,18 +1,35 @@
 'use strict';
 
 angular.module('womenWorkingWithWomenApp')
-  .controller('NavBarCtrl', function($scope, $timeout){
+  .controller('NavBarCtrl', ['$scope', '$timeout', '$location', function($scope, $timeout, $location){
     $timeout(function(){
       $(".dropdown-button").dropdown();
       $('.button-collapse').sideNav({
-            closeOnClick: false, 
+            closeOnClick: false,
             edge: 'right'
           }
       );
       $('.collapsible').collapsible();
     });
 
+    // See what route is active
+    $scope.routeIs = function(item){
+      if($location.path() == item){
+        return true;
+      }
+      return false;
+    }
+
+    $scope.routeContains = function(item){
+      console.log(item);
+      if($location.path().indexOf(item) != -1){
+        return true;
+      }
+      return false;
+    }
+
+
     $scope.closeNav = function(){
       $('.button-collapse').sideNav('hide');
     }
-});
+}]);
