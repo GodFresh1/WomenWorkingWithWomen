1fe1e64699de807133f58d35147c34a8c09cd295
golliher-sidney <golliher.sidney@gmail.com>
2016-02-19 12:23:05 -0500

    added the controllers and css to gallery

---
+++ client/app/content/gallery/galleryFactory.js
@@ -0,0 +1,324 @@
+
+angular.module('womenWorkingWithWomenApp')
+  .factory('Gallery', function(){
+    return  [
+      {
+        name: "1",
+        imgUrl: "http://i.imgur.com/o5IIeCn.jpg"
+      },
+      {
+        name: "2",
+        imgUrl: "http://i.imgur.com/SLTIRq4.jpg"
+      },
+      {
+        name: "3",
+        imgUrl: "http://i.imgur.com/UTuniF8.jpg"
+      },
+      {
+        name: "4",
+        imgUrl: "http://i.imgur.com/4YJl1Mt.jpg"
+      },
+      {
+        name: "5",
+        imgUrl: "http://i.imgur.com/rmBDmMg.png"
+      },
+      {
+        name: "6",
+        imgUrl: "http://i.imgur.com/dPNkZzB.jpg"
+      },
+      {
+        name: "7",
+        imgUrl: "http://i.imgur.com/BgdMV1P.jpg"
+      },
+      {
+        name: "8",
+        imgUrl: "http://i.imgur.com/LirbQnq.jpg"
+      },
+      {
+        name: "9",
+        imgUrl: "http://i.imgur.com/QDpjA4B.jpg"
+      },
+      {
+        name: "10",
+        imgUrl: "http://i.imgur.com/1GHpQnV.jpg"
+      },
+      {
+        name: "11",
+        imgUrl: "http://i.imgur.com/hI4hhJ6.jpg"
+      },
+      {
+        name: "12",
+        imgUrl: "http://i.imgur.com/5yv5EzQ.jpg"
+      },
+      {
+        name: "13",
+        imgUrl: "http://i.imgur.com/vSZ9gT6.jpg"
+      },
+      {
+        name: "14",
+        imgUrl: "http://i.imgur.com/3bWeVai.jpg"
+      },
+      {
+        name: "15",
+        imgUrl: "http://i.imgur.com/P3hkKbH.jpg"
+      },
+      {
+        name: "16",
+        imgUrl: "http://i.imgur.com/442t7do.jpg"
+      },
+      {
+        name: "17",
+        imgUrl: "http://i.imgur.com/aaxgucO.jpg"
+      },
+      {
+        name: "18",
+        imgUrl: "http://i.imgur.com/DpO2egB.jpg"
+      },
+      {
+        name: "19",
+        imgUrl: "http://i.imgur.com/Q2ci3tN.jpg"
+      },
+      {
+        name: "20",
+        imgUrl: "http://i.imgur.com/SIyAQZV.jpg"
+      },{
+        name: "21",
+        imgUrl: "http://i.imgur.com/SdsM5fX.jpg"
+      },
+      {
+        name: "22",
+        imgUrl: "http://i.imgur.com/HAdnSaF.jpg"
+      },
+      {
+        name: "23",
+        imgUrl: "http://i.imgur.com/V5CTeYh.jpg"
+      },
+      {
+        name: "24",
+        imgUrl: "http://i.imgur.com/CDpE3Rf.jpg"
+      },
+      {
+        name: "25",
+        imgUrl: "http://i.imgur.com/lsCxNsQ.jpg"
+      },
+      {
+        name: "26",
+        imgUrl: "http://i.imgur.com/MvvIFSs.jpg"
+      },
+      {
+        name: "27",
+        imgUrl: "http://i.imgur.com/7d3gsQD.jpg"
+      },
+      {
+        name: "28",
+        imgUrl: "http://i.imgur.com/4FoPUnm.jpg"
+      },
+      {
+        name: "29",
+        imgUrl: "http://i.imgur.com/2wXAuXv.jpg"
+      },
+      {
+        name: "30",
+        imgUrl: "http://i.imgur.com/C6lweU5.jpg"
+      },{
+        name: "31",
+        imgUrl: "http://i.imgur.com/ofTHBSN.jpg"
+      },
+      {
+        name: "32",
+        imgUrl: "http://i.imgur.com/uier7j9.jpg"
+      },
+      {
+        name: "33",
+        imgUrl: "http://i.imgur.com/AbsL85V.jpg"
+      },
+      {
+        name: "34",
+        imgUrl: "http://i.imgur.com/lKF7k5K.jpg"
+      },
+      {
+        name: "35",
+        imgUrl: "http://i.imgur.com/zihNVTE.jpg"
+      },
+      {
+        name: "36",
+        imgUrl: "http://i.imgur.com/jZoSRZq.jpg"
+      },
+      {
+        name: "37",
+        imgUrl: "http://i.imgur.com/t29xCk3.jpg"
+      },
+      {
+        name: "38",
+        imgUrl: "http://i.imgur.com/mp92SXb.jpg"
+      },
+      {
+        name: "39",
+        imgUrl: "http://i.imgur.com/Er7hz6e.jpg"
+      },
+      {
+        name: "40",
+        imgUrl: "http://i.imgur.com/eOwuACI.jpg"
+      },{
+        name: "41",
+        imgUrl: "http://i.imgur.com/cl8klys.jpg"
+      },
+      {
+        name: "42",
+        imgUrl: "http://i.imgur.com/MmFf1fY.jpg"
+      },
+      {
+        name: "43",
+        imgUrl: "http://i.imgur.com/nAGIpsh.jpg"
+      },
+      {
+        name: "44",
+        imgUrl: "http://i.imgur.com/fCNoYjN.jpg"
+      },
+      {
+        name: "45",
+        imgUrl: "http://i.imgur.com/h2LOqxq.jpg"
+      },
+      {
+        name: "46",
+        imgUrl: "http://i.imgur.com/WA7vYH8.jpg"
+      },
+      {
+        name: "47",
+        imgUrl: "http://i.imgur.com/GIacYYG.jpg"
+      },
+      {
+        name: "48",
+        imgUrl: "http://i.imgur.com/2XHnGY5.jpg"
+      },
+      {
+        name: "49",
+        imgUrl: "http://i.imgur.com/SvXzySY.jpg"
+      },
+      {
+        name: "50",
+        imgUrl: "http://i.imgur.com/vNp6kUF.jpg"
+      },{
+        name: "51",
+        imgUrl: "http://i.imgur.com/3DDGotZ.jpg"
+      },
+      {
+        name: "52",
+        imgUrl: "http://i.imgur.com/sPUX6fF.jpg"
+      },
+      {
+        name: "53",
+        imgUrl: "http://i.imgur.com/U1lUHDJ.jpg"
+      },
+      {
+        name: "54",
+        imgUrl: "http://i.imgur.com/APu2nKb.jpg"
+      },
+      {
+        name: "55",
+        imgUrl: "http://i.imgur.com/fVprCEp.jpg"
+      },
+      {
+        name: "56",
+        imgUrl: "http://i.imgur.com/NpAeav7.jpg"
+      },
+      {
+        name: "57",
+        imgUrl: "http://i.imgur.com/6XoMbsA.jpg"
+      },
+      {
+        name: "58",
+        imgUrl: "http://i.imgur.com/XPqM467.jpg"
+      },
+      {
+        name: "59",
+        imgUrl: "http://i.imgur.com/20H9beG.jpg"
+      },
+      {
+        name: "60",
+        imgUrl: "http://i.imgur.com/wcTiUzt.jpg"
+      },{
+        name: "61",
+        imgUrl: "http://i.imgur.com/IV3kskm.jpg"
+      },
+      {
+        name: "62",
+        imgUrl: "http://i.imgur.com/doPtimy.jpg"
+      },
+      {
+        name: "63",
+        imgUrl: "http://i.imgur.com/MErT78f.jpg"
+      },
+      {
+        name: "64",
+        imgUrl: "http://i.imgur.com/RYQi9uE.jpg"
+      },
+      {
+        name: "65",
+        imgUrl: "http://i.imgur.com/GrUp1xc.jpg"
+      },
+      {
+        name: "66",
+        imgUrl: "http://i.imgur.com/LRVJoVJ.jpg"
+      },
+      {
+        name: "67",
+        imgUrl: "http://i.imgur.com/zzakptq.jpg"
+      },
+      {
+        name: "68",
+        imgUrl: "http://i.imgur.com/GpgIhEw.jpg"
+      },
+      {
+        name: "69",
+        imgUrl: "http://i.imgur.com/NCyGX9M.jpg"
+      },
+      {
+        name: "70",
+        imgUrl: "http://i.imgur.com/s6JTF8y.jpg"
+      },{
+        name: "71",
+        imgUrl: "http://i.imgur.com/XqEdlKr.jpg"
+      },
+      {
+        name: "72",
+        imgUrl: "http://i.imgur.com/cH8Jf5V.jpg"
+      },
+      {
+        name: "73",
+        imgUrl: "http://i.imgur.com/z1GCEKL.jpg"
+      },
+      {
+        name: "74",
+        imgUrl: "http://i.imgur.com/bErR5UL.png"
+      },
+      {
+        name: "75",
+        imgUrl: "http://i.imgur.com/XZ6kDp7.png"
+      },
+      {
+        name: "76",
+        imgUrl: "http://i.imgur.com/SzhgR4Q.png"
+      },
+      {
+        name: "77",
+        imgUrl: "http://i.imgur.com/2dEWy2g.png"
+      },
+      {
+        name: "78",
+        imgUrl: "http://i.imgur.com/zrg9dkZ.png"
+      },
+      {
+        name: "79",
+        imgUrl: "http://i.imgur.com/r2T5Jl9.png"
+      },
+      {
+        name: "80",
+        imgUrl: "http://i.imgur.com/xtb5Vfo.png"
+      },
+      {
+        name: "81",
+        imgUrl: "http://i.imgur.com/ekHMD2s.png"
+      }
+    ]
+  });
