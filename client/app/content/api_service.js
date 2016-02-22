9feb3bb7040dc26920b3f1ecc35a87a2a761ee0b
bmtreuherz <bmtreuherz@ufl.edu>
2016-02-21 19:05:41 -0500

    finished partner and conteact request forms

--- client/app/content/api_service.js
+++ client/app/content/api_service.js
@@ -3,6 +3,9 @@ angular.module('womenWorkingWithWomenApp')
     return {
       partnerRequest: function(partner){
         return $http.post('http://localhost:9000/api/partners/submit-request', partner);
+      },
+      contactRequest: function(request){
+        return $http.post('http://localhost:9000/api/contact/submit-request', request);
       }
     }
 }]);
