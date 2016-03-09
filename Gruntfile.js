2c2ba6b9171c1ebbff706718ab723e60ab20241e
Luis Carbonell <lt.carbonell@yahoo.com>
2016-03-09 18:41:22 -0500

    Fixed port number

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 9000
       },
       dev: {
         options: {
@@ -193,7 +193,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: process.env.PORT || 3000
+            PORT: process.env.PORT || 900
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
--- client/app/content/events/events.controller.js
+++ client/app/content/events/events.controller.js
@@ -2,22 +2,34 @@
 
 
 angular.module('womenWorkingWithWomenApp')
-  .controller('EventsCtrl', ['$scope', '$compile', '$timeout', 'uiCalendarConfig', function($scope, $compile, $timeout, uiCalendarConfig) {
+  .controller('EventsCtrl', ['$scope', '$compile', '$timeout', 'uiCalendarConfig', 'Api', function($scope, $compile, $timeout, uiCalendarConfig, Api) {
   $scope.title = "How to Get Involved";
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
+    var allEvents = [];
+
+    Api.getAllEvents().then(function(response){
+      $scope.eventSources = reponse.Data;
+    }, function(error){
+      // handle error here
+    });
 
     $scope.eventSources = {
-       color: '#EFE5F0',
-       textColor: 'purple',
-       events: [
-          {title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {title: 'All day Lunch',start: new Date(y, m, 25, 12, 0),end: new Date(y, m, 25, 14, 0),allDay: true},
-          {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
+      //  color: '#EFE5F0',
+      //  textColor: 'purple',
+       events: [{
+         id: 0,
+         title: "Test Event",
+         start: new Date(2016, 3, 17, 2, 30, 0),
+         end: new Date(2016, 3, 17, 4, 30, 0), // Note: this is a 2 hour event.
+         description: "An event for testing.",
+         location: "1234 Made Up St, Doesnt Exist, AL, 12345",
+         imgUrl: "http://notarealurl.com"
+       }]
     };
+
     $scope.uiConfig = {
       calendar:{
         editable: true,
