a57545536221908c97158718e11847617b5347b1
bmtreuherz <bmtreuherz@ufl.edu>
2016-03-08 19:41:38 -0500

    Finished backend for events.

--- server/routes.js
+++ server/routes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
   app.use('/api/users', require('./api/user'));
   app.use('/api/partners', require('./api/partner'));
   app.use('/api/contact', require('./api/contact'));
+  app.use('/api/events', require('./api/event'));
 
   // app.use('/api/contact', require('./api/contact'));
 
