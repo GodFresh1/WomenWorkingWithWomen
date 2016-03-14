839b7d453a677d4109844c82241be13446ec251f
Luis Carbonell <lt.carbonell@yahoo.com>
2016-03-14 09:02:26 -0400

    Finished forms and pulling events to calendar and to registration forms

--- client/app/content/events/events.controller.js
+++ client/app/content/events/events.controller.js
@@ -11,24 +11,75 @@ angular.module('womenWorkingWithWomenApp')
     var allEvents = [];
 
     Api.getAllEvents().then(function(response){
-      $scope.eventSources = reponse.Data;
+       $scope.events=response.data;
+       console.log(allEvents);
     }, function(error){
       // handle error here
     });
 
     $scope.eventSources = {
-      //  color: '#EFE5F0',
-      //  textColor: 'purple',
-       events: [{
-         id: 0,
-         title: "Test Event",
-         start: new Date(2016, 3, 17, 2, 30, 0),
-         end: new Date(2016, 3, 17, 4, 30, 0), // Note: this is a 2 hour event.
-         description: "An event for testing.",
-         location: "1234 Made Up St, Doesnt Exist, AL, 12345",
-         imgUrl: "http://notarealurl.com"
-       }]
+        color: '#EFE5F0',
+        textColor: 'purple',
+        events: $scope.events
+       //console.log(events);
     };
+    /* alert on eventClick */
+    $scope.alertOnEventClick = function( date, jsEvent, view){
+        $scope.alertMessage = (date.title + ' was clicked ');
+    };
+    /* alert on Drop */
+     $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
+       $scope.alertMessage = ('Event Dropped to make dayDelta ' + delta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
+       $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    };
+    /* add and removes an event source of choice */
+    $scope.addRemoveEventSource = function(sources,source) {
+      var canAdd = 0;
+      angular.forEach(sources,function(value, key){
+        if(sources[key] === source){
+          sources.splice(key,1);
+          canAdd = 1;
+        }
+      });
+      if(canAdd === 0){
+        sources.push(source);
+      }
+    };
+    /* add custom event*/
+    $scope.addEvent = function() {
+      $scope.events.push({
+        title: 'Open Sesame',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        className: ['openSesame']
+      });
+    };
+    /* remove event */
+    $scope.remove = function(index) {
+      $scope.events.splice(index,1);
+    };
+    /* Change View */
+    $scope.changeView = function(view,calendar) {
+      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
+    };
+    /* Change View */
+    $scope.renderCalender = function(calendar) {
+      $timeout(function() {
+        if(uiCalendarConfig.calendars[calendar]){
+          uiCalendarConfig.calendars[calendar].fullCalendar('render');
+        }
+      });
+    };
+     /* Render Tooltip */
+    $scope.eventRender = function( event, element, view ) {
+        element.attr({'tooltip': event.title,
+                      'tooltip-append-to-body': true});
+        $compile(element)($scope);
+    };
+
 
     $scope.uiConfig = {
       calendar:{
