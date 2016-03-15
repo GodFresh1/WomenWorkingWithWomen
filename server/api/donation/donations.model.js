0de4cae2232df7a817d735abeca3d0d7c9ca1e4f
Jayson <jaysalkey@gmail.com>
2016-03-15 13:02:03 -0400

    register Donations

---
+++ server/api/donation/donations.model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var DonationSchema = new Schema({
+  // _id: {type:String, required:true}, // The id will be the attendee's email address.
+  firstName: {type:String, required:true},
+  lastName: {type:String, required:true},
+  amount: {type:Number, required:true},
+  level: {type:String, required:true},
+});
+
+DonationSchema.virtual('name').get(function(){
+  return this.firstName + " " + this.lastName;
+})
+
+module.exports = mongoose.model('Donation', DonationSchema);
---
+++ server/api/donation/donations.spec.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var should = require('should');
+var app = require('../../app');
+var request = require('supertest');
+var Donation = require('./donation.model');
+
+var donation = new Donation({
+  _id: "swag@swag.com",
+  firstName: "John",
+  lastName: "Smith",
+  amount: 12345678,
+  level: "diamond"
+});
+
+describe('Donation Model', function() {
+  before(function(done) {
+    // Clear donations before testing
+    Donation.remove().exec().then(function() {
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    // Set the donation to this before each test is ran.
+    donation = new Donation({
+      _id: "swag@swag.com",
+      firstName: "John",
+      lastName: "Smith",
+      amount: 12345678,
+      level: "diamond"
+    });
+    done();
+  });
+
+  afterEach(function(done) {
+    Donation.remove().exec().then(function() {
+      done();
+    });
+  });
+  it('should respond with JSON array when reqesting /api/donations', function(done) {
+      request(app)
+        .get('/api/donations') // donations
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });
+
+  it('should fail when saving a duplicate donation', function(done) {
+    donation.save(function() {
+      var donationDup = new Donation(donation);
+      donationDup.save(function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
+
+  it('should fail when saving without an id', function(done) {
+    donation._id = '';
+    donation.save(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('should fail when saving without a first name', function(done) {
+    donation.firstName = '';
+    donation.save(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('should fail when saving without a last name', function(done) {
+    donation.lastName = '';
+    donation.save(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('should pass when saving properly', function(done) {
+    donation.save(function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+});
