46ffe4948a695748d9d411e19a596352777d74a5
Luis Carbonell <lt.carbonell@yahoo.com>
2016-03-16 18:26:31 -0400

    connected vendor forms to backend

--- server/api/event/index.js
+++ server/api/event/index.js
@@ -10,6 +10,7 @@ router.get('/:id', controller.getOne);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.put('/:id/addAttendee', controller.addAttendee);
+router.put('/:id/addVendor', controller.addVendor);
 router.delete('/:id', controller.destroy);
 
 module.exports = router;
