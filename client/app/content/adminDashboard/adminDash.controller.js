0883714df872564f99795c6551652386749f19f6
bmtreuherz <bmtreuherz@ufl.edu>
2016-04-04 19:18:35 -0400

    Started creating a dialog to add new events.

--- client/app/content/adminDashboard/adminDash.controller.js
+++ client/app/content/adminDashboard/adminDash.controller.js
@@ -2,13 +2,13 @@
 
 
 angular.module('womenWorkingWithWomenApp')
-  .controller('AdminDashCtrl', ['$scope', 'Api', '$mdToast', 'Auth', '$mdDialog', function($scope, Api, $mdToast, Auth, $mdDialog) {
+  .controller('AdminDashCtrl', ['$scope', 'Api', '$mdToast', 'Auth', '$mdDialog', '$mdMedia', function($scope, Api, $mdToast, Auth, $mdDialog, $mdMedia) {
     $scope.events = [];
     $scope.showDetails = {};
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
-
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
     Api.getAllEvents().then(function(response){
       $scope.events = response.data;
@@ -26,6 +26,29 @@ angular.module('womenWorkingWithWomenApp')
     $scope.showDetails = function(event){
       var isShown = $scope.showDetails[event._id];
       $scope.showDetails[event._id] = (isShown == undefined || !isShown) ? true : false;
+    }
+
+    $scope.createEvent = function($event){
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+       $mdDialog.show({
+         controller: DialogController,
+         templateUrl: '/app/content/adminDashboard/newEventDialog.html',
+         parent: angular.element(document.body),
+         targetEvent: $event,
+         clickOutsideToClose:true,
+         fullscreen: useFullScreen
+       })
+       .then(function(event) {
+         // Add the event to the database.
+         console.log(event);
+       }, function() {
+         concole.log("add event canceled.")
+       });
+       $scope.$watch(function() {
+         return $mdMedia('xs') || $mdMedia('sm');
+       }, function(wantsFullScreen) {
+         $scope.customFullscreen = (wantsFullScreen === true);
+       });
     }
 
     $scope.checkBox = function(attendee, $event){
@@ -84,5 +107,19 @@ angular.module('womenWorkingWithWomenApp')
           });
         }
       }
+    };
+
+    function DialogController($scope, $mdDialog) {
+      $scope.event = {};
+
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+      $scope.create = function() {
+        $mdDialog.hide($scope.event);
+      };
     };
 }]);
