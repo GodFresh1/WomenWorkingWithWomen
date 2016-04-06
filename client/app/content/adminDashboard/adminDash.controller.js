54018de626a4404f08de8daae514bd1edf5a5eae
golliher-sidney <golliher-sidney@gmail.com>
2016-04-05 20:06:50 -0400

    my commit for beginning the CSV function

--- client/app/content/adminDashboard/adminDash.controller.js
+++ client/app/content/adminDashboard/adminDash.controller.js
@@ -5,6 +5,7 @@ angular.module('womenWorkingWithWomenApp')
   .controller('AdminDashCtrl', ['$scope', 'Api', '$mdToast', 'Auth', '$mdDialog', function($scope, Api, $mdToast, Auth, $mdDialog) {
     $scope.events = [];
     $scope.showDetails = {};
+    $scope.csvEventTemp = [];
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
@@ -53,6 +54,28 @@ angular.module('womenWorkingWithWomenApp')
       $scope.showDetails[event._id] = (isShown == undefined || !isShown) ? true : false;
     }
 
+    $scope.produceCSV = function(event){
+      Api.getOneEvent(event._id).then(function(response){
+        $scope.csvEventtTemp = response.data;
+        /*Produce and commence download for CSV*/
+        /*https://github.com/zemirco/json2csv*/
+        var json2csv = require('json2csv');
+        var fields = ['title', 'description', 'location', 'startDate', 'startTime', 'endDate', 'endTime', 'attendees', 'vendors', 'volunteers'];
+
+        json2csv({ data: Object, fields: fields }, function(err, csv) {
+          if (err) console.log(err);
+          console.log(csv);
+        });
+      }, function(err){
+        $mdToast.show(
+          $mdToast.simple()
+            .content('Error: Could not connect to the server. ')
+            .position('top right')
+            .hideDelay(3000)
+            .theme("error-toast")
+          );
+      });
+    }
 
     $scope.checkBox = function(attendee, $event){
       $event.preventDefault();
