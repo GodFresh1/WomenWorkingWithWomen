b5eedcd95120c65ed18d45eafa6a17567cd94334
Jayson Salkey <jaysalkey@gmail.com>
2016-04-17 23:34:25 -0400

    more changes

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -37,12 +37,12 @@ var all = {
 
   // MongoDB connection options
   mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
+    uri: 'mongodb://dev:dev@ds055515.mongolab.com:55515/women_working_with_women'
   },
+  seedDB: false,
+  smtp: {
+      uri: 'smtps://womenworkingcontactrequest%40gmail.com:womenworkingwithwomen@smtp.gmail.com'
+    }
 
 };
 
