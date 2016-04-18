f7ff0716697fee524e22f1954b6743ec48ccfa2f
bmtreuherz <bmtreuherz@ufl.edu>
2016-04-18 16:00:12 -0400

    Changed API_Service base url again again

--- client/app/content/api_service.js
+++ client/app/content/api_service.js
@@ -2,7 +2,7 @@
 
 angular.module('womenWorkingWithWomenApp')
   .factory('Api', ['$http', function($http){
-    var API_BASE_URL = 'https://thawing-dusk-72900.herokuapp.com/';
+    var API_BASE_URL = 'https://thawing-dusk-72900.herokuapp.com/api/';
     return {
       // Contact Requests
       partnerRequest: function(partner){
