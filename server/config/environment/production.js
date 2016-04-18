43c123ece2f23f7bd1227008974b2d62718b95eb
Jayson Salkey <jaysalkey@gmail.com>
2016-04-17 23:24:56 -0400

    more changes

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,21 +3,32 @@
 // Production specific configuration
 // =================================
 module.exports = {
+
+  /* PRODUCTION SHIT*/
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  // ip:       process.env.OPENSHIFT_NODEJS_IP ||
+  //           process.env.IP ||
+  //           undefined,
+  //
+  // // Server port
+  // port:     process.env.OPENSHIFT_NODEJS_PORT ||
+  //           process.env.PORT ||
+  //           8080,
+  //
+  // // MongoDB connection options
+  // mongo: {
+  //   uri:    process.env.MONGOLAB_URI ||
+  //           process.env.MONGOHQ_URL ||
+  //           process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+  //           'mongodb://localhost/womenworkingwithwomen'
+  // }
 
-  // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
-
-  // MongoDB connection options
+  /*DEV SHIT*/
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/womenworkingwithwomen'
-  }
+    uri: 'mongodb://dev:dev@ds055515.mongolab.com:55515/women_working_with_women'
+  },
+  seedDB: false,
+  smtp: {
+      uri: 'smtps://womenworkingcontactrequest%40gmail.com:womenworkingwithwomen@smtp.gmail.com'
+    }
 };
