f27261d1d1eb59a12c5f937a4d57158fdb05a4a8
Jayson <jaysalkey@gmail.com>
2016-04-21 22:44:30 -0400

    Variable pricing added for Events

--- client/app/content/events/vendor/vendor.controller.js
+++ client/app/content/events/vendor/vendor.controller.js
@@ -31,7 +31,7 @@ angular.module('womenWorkingWithWomenApp')
       });
       $mdDialog.show( alert ).finally(function() {
             Api.getOneEvent($scope.attendee.eventAttending).then(function(response){
-              console.log(response);
+              console.log(response.data.vendor_price);
               alert = undefined;
               var f = document.createElement("form");
               f.setAttribute('method',"post");
@@ -56,7 +56,7 @@ angular.module('womenWorkingWithWomenApp')
               var amount = document.createElement("input"); //input element, Submit button
               amount.setAttribute('type',"text");
               amount.setAttribute('name','amount');
-              amount.setAttribute('value', response.vendor_price.toString());
+              amount.setAttribute('value', response.data.vendor_price.toString());
               var no_shipping = document.createElement("input"); //input element, Submit button
               no_shipping.setAttribute('type',"hidden");
               no_shipping.setAttribute('name','no_shipping');
