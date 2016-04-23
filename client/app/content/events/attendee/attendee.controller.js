65d5bd3c416bbffe554cc8760dc45433a62468bd
Annie Luc <annhiluc@gmail.com>
2016-04-23 15:29:31 -0400

    Add multiple registrations

--- client/app/content/events/attendee/attendee.controller.js
+++ client/app/content/events/attendee/attendee.controller.js
@@ -7,7 +7,7 @@ angular.module('womenWorkingWithWomenApp')
     $scope.events = [];
     $scope.genders = ('Male Female Other').split(' ');
     $scope.fashions = ('Yes No').split(' ');
-    $scope.registrations.number = 0;
+    $scope.registrations;
 
     // Get all the events from the server.
     Api.getAllEvents().then(function(response){
