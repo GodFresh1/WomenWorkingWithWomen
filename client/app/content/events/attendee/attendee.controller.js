be0a00f85b70956cfb906c9a0daf81127844c085
Annie Luc <annhiluc@gmail.com>
2016-04-24 11:02:28 -0400

    Add multiple registrations

--- client/app/content/events/attendee/attendee.controller.js
+++ client/app/content/events/attendee/attendee.controller.js
@@ -39,7 +39,7 @@ angular.module('womenWorkingWithWomenApp')
         });
 
         if(index == $scope.attendee.length){
-          handleSuccess();
+          //handleSuccess();
         }
       }, function(error){
         handleError(error);
